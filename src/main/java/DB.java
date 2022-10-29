8bad458c4f8d50fe13b0911dc0870d9c700296ef
Meirbek Bolatbekov <76254028+meirbnb@users.noreply.github.com>
2022-10-29 20:14:23 +0300

    Update DB.java

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
-    private final String url = "postgres://kgtudgtcfazgwh:10f3b7de6744921595f2c5d6443d0ba0f73cdc8254fdbfaa5313ed6507ba9e52@ec2-52-214-125-106.eu-west-1.compute.amazonaws.com:5432/d2j6bj1jao3ldi";
-    private final String user = "atjlyfnvblurju";
-    private final String password = "adfcc3481cd6f12b56c3eb6f33f30c5ccd34ed43450cf6e426643db6a173337a";
+    private final String url = "postgres://put_here_link_to_your_postgres_DB";
+    private final String user = "admin";
+    private final String password = "admin";
 /*
     private final String url = "jdbc:postgresql://localhost/quizent";
     private final String user = "postgres";
